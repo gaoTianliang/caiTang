27321cc506c7577ebbed71e59891ed87a15f1e96
gaoTianLiang <gaoTianLiang>
2020-04-01 16:09:50 +0800

    正则表达式获取html页面里，纯文本内容

--- src/main/java/com/code/core/regex/RemoveHtml.java
+++ src/main/java/com/code/core/regex/RemoveHtml.java
@@ -1,19 +1,16 @@
 package com.code.core.regex;
 
-public class RemoveHtml {
+import org.junit.Test;
 
-    public static void main(String[] args) {
-        String htmlStr = "<p>46784<strong>46784</strong></p>";
-        System.out.println(delHtmlTags(htmlStr));
-    }
+public class RemoveHtml {
 
     /**
      * 去除html代码中含有的标签
-     *
-     * @param htmlStr
-     * @return
      */
-    public static String delHtmlTags(String htmlStr) {
+    @Test
+    public void delHtmlTags() {
+
+        String htmlStr = "<p>46784<strong>46784</strong></p>";
 
         //定义script的正则表达式，去除js可以防止注入
         String scriptRegex = "<script[^>]*?>[\\s\\S]*?<\\/script>";
@@ -24,30 +21,24 @@ public class RemoveHtml {
         //定义空格,回车,换行符,制表符
         String spaceRegex = "\\s*|\t|\r|\n";
 
+        System.out.println("原始数据");
+        System.out.println(htmlStr);
         // 过滤script标签
         htmlStr = htmlStr.replaceAll(scriptRegex, " ");
+        System.out.println("过滤script标签");
+        System.out.println(htmlStr);
         // 过滤style标签
         htmlStr = htmlStr.replaceAll(styleRegex, " ");
+        System.out.println("过滤style标签");
+        System.out.println(htmlStr);
         // 过滤html标签
         htmlStr = htmlStr.replaceAll(htmlRegex, " ");
+        System.out.println("过滤html标签");
+        System.out.println(htmlStr);
         // 过滤空格等
-//        htmlStr = htmlStr.replaceAll(spaceRegex, "");
-        return htmlStr.trim(); // 返回文本字符串
-    }
-
-    /**
-     * 获取HTML代码里的内容
-     *
-     * @param htmlStr
-     * @return
-     */
-
-    public static String getTextFromHtml(String htmlStr) {
-        //去除html标签
-        htmlStr = delHtmlTags(htmlStr);
-        //去除空格" "
-        htmlStr = htmlStr.replaceAll(" ", "");
-        return htmlStr;
+        htmlStr = htmlStr.replaceAll(spaceRegex, "");
+        System.out.println("过滤空格等");
+        System.out.println(htmlStr);
     }
 }
 
